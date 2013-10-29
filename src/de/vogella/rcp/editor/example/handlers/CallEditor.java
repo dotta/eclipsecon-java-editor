88890c71b7026f623845dd39be97c02118b91420
Mirco Dotta <mirco.dotta@typesafe.com>
2013-10-29 14:09:10 +0100

    working

---
+++ src/de/vogella/rcp/editor/example/handlers/CallEditor.java
@@ -0,0 +1,47 @@
+package de.vogella.rcp.editor.example.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import de.vogella.rcp.editor.example.MyPersonEditor;
+import de.vogella.rcp.editor.example.MyPersonEditorInput;
+import de.vogella.rcp.editor.example.View;
+import de.vogella.rcp.editor.example.model.Person;
+
+public class CallEditor extends AbstractHandler {
+
+  @Override
+  public Object execute(ExecutionEvent event) throws ExecutionException {
+    System.out.println("called");
+    // get the view
+    IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
+    IWorkbenchPage page = window.getActivePage();
+    View view = (View) page.findView(View.ID);
+    // get the selection
+    ISelection selection = view.getSite().getSelectionProvider()
+        .getSelection();
+    if (selection != null && selection instanceof IStructuredSelection) {
+      Object obj = ((IStructuredSelection) selection).getFirstElement();
+      // if we had a selection lets open the editor
+      if (obj != null) {
+        Person person = (Person) obj;
+        MyPersonEditorInput input = new MyPersonEditorInput(person.getId());
+        try {
+          page.openEditor(input, MyPersonEditor.ID);
+
+        } catch (PartInitException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    }
+    return null;
+  }
+
+} 
---
+++ src/editor/java/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,20 @@
+package editor.java;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "editor.java.perspective";
+
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
